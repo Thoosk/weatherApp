f0d405f1011c7d5fc73b685a00553a20b2b7159b
Thoosk <thomas.skant@gmx.at>
2021-03-05 17:04:12 +0100

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,28 +12,18 @@ module.exports = {
   module: {
     rules: [
       {
-        // this rule will run babel over all our .js files, except for the ones in node_modules
-        // babel ensures that older browsers can run our page too
-        // still need to install babel with npm (npm i @babel/core @babel/preset-env babel-loader)
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          // without additional settings, this will reference .babelrc
           loader: "babel-loader",
         },
       },
       {
-        // adding a loader for your css files
-        // you first have to install it via npm (npm install --save-dev style-loader css-loader)
-        // runs the style-loader and css-loader against all css files
-        // you also need to import the css in your index.js in src
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
 
       {
-        // adding a processor for your images
-        // will run through all the below mentioned files and process them through the asset manager
         test: /\.(png|jpg|jpeg)$/i,
         type: "asset/resource",
       },
